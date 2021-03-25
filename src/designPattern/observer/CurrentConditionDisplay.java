50739f3b969657de0657211cf604e57a41ee7545
wammelier <royhan4417@gmail.com>
2021-03-25 23:55:57 +0900

    designPattern observer study

---
+++ src/designPattern/observer/CurrentConditionDisplay.java
@@ -0,0 +1,26 @@
+package designPattern.observer;
+
+/**
+ * @Author Seungwhan Lee(sw4417@into.guru)
+ */
+public class CurrentConditionDisplay implements Observer, DisplayElement {
+    private float temperature;
+    private float humidity;
+    private Subject weatherData;
+
+    public CurrentConditionDisplay(Subject weatherData) {
+        this.weatherData = weatherData;
+        weatherData.registerObserver(this);
+    }
+
+    public void update(float temperature, float humidity, float pressure) {
+        this.temperature = temperature;
+        this.humidity = humidity;
+        display();
+    }
+    
+    public void display() {
+        System.out.println("Current conditions: " + temperature + "F degrees and " + humidity + " % humidity");
+
+    }
+}
